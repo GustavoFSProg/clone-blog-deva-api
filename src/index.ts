7267103caedab65326742ed4546477f071eaba31
Gustavo <gustavo.prog40@gmail.com>
2022-08-05 15:56:20 -0300

    again

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,8 @@ app.use(express.json())
 // **** O codigo abiaxo setou o cors ****d
 
 
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://www.netlify.com/")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-});
-
 app.use(route)
-
+app.use(cors())
 
 app.get('/', function (req: Request, res: Response) {
   return res.status(200).send({ msg: ` 🍏 Api Running` })
